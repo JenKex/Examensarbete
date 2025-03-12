219c0e0ba2fafffc4a87c231dcb2ce5d6a428543
Anna <acarlssong@hotmail.com>
2025-03-12 13:27:20 +0100

    Gjort bakgrundsbild seamless; OK för presentation

--- src/pages/Hem.jsx
+++ src/pages/Hem.jsx
@@ -20,17 +20,14 @@ export function Hem() {
                 </ul>
                 <div className="blurb-button-container">
                     <button onClick={() => navigate('/meny')}>Se vår meny</button>
-                    {/* Styla externa a-linkar att likna och bete sig som buttons */}
-                    <button><a href="https://www.foodora.se/restaurant/ykxd/150g-burgers" target="_blank">Beställ från Foodora</a></button>
+                    {/* <a className="button-link" href="https://www.foodora.se/restaurant/ykxd/150g-burgers" target="_blank">Beställ från Foodora</a> */}
+                    <button><a href="https://www.foodora.se/restaurant/ykxd/150g-burgers" target="_blank">Beställ från Foodora</a></button>                
                 </div>
             </div>
             <div className="front-page-teasers">
                 <div className="front-page-box-rounded-left">
                     <h4>Burgarhungrig?</h4>
-                    {/* <p>→</p> */}
-                    {/* Working well; write a classname for 'fake links' supposed to look like buttons */}
                     <button className="teaser-button"><HashLink smooth to="/meny#burgare">VÅRA BURGARE</HashLink></button>
-                    {/* <HashLink smooth to="/meny#burgare">Våra Burgare</HashLink> */}
                 </div>
                 <div className="front-page-image-burger">
                 </div>
@@ -38,9 +35,7 @@ export function Hem() {
                 </div>
                 <div className="front-page-box-rounded-right">
                     <h4>Pizzasugen?</h4>
-                    {/* <p>←</p> */}
                     <button className="teaser-button"><HashLink smooth to="/meny#pizzor">VÅRA PIZZOR</HashLink></button>
-                    {/* <HashLink smooth to="/meny#pizzor">Våra Pizzor</HashLink> */}
                 </div>
             </div>
         </div>
